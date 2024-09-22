2cb08e6609ca410e39b029fa1db6a1cc838e074c
Adellar Irankunda <addyirankunda@gmail.com>
2024-09-22 08:27:23 -0700

    Initial Commit

---
+++ physics.solvers/physics_solversApp.swift
@@ -0,0 +1,17 @@
+//
+//  physics_solversApp.swift
+//  physics.solvers
+//
+//  Created by Adellar Irankunda on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct physics_solversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
